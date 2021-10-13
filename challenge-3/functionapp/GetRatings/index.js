082a4d00efa3ebc1503d1613450f8cda06f7a7d6
Ana Del Campo Mendizabal <andelcam@microsoft.com>
2021-10-13 13:08:28 -0400

    commit on 'main'

--- challenge-3/functionapp/GetRatings/index.js
+++ challenge-3/functionapp/GetRatings/index.js
@@ -1,12 +1,41 @@
+const axios = require('axios');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const userId = (req.query.userId || (req.body && req.body.userId));
 
-   var documents = context.bindings.userId;
-   var results = JSON.stringify(documents); 
+    var url = `https://serverlessohapi.azurewebsites.net/api/GetUser?userId=${userId}`
+    var results = "";
+
+    await axios.get(url)
+        .then(function (response) {
+            context.log(response.data)
+            currentStatus = response.status
+        })
+        .catch(function (error) {
+            context.log(error.response.data);
+            currentStatus = 404
+            results = error.response.data
+        });
+        
+        if (currentStatus == 200) { 
+                var documents = context.bindings.userId;
+                if (documents.length < 1)
+                {
+                    currentStatus = 404;
+                }
+                else{
+                    results = JSON.stringify(documents); 
+                    currentStatus = 200;
+                }
+
+            }
+
+   
 
     context.res = {       
-        body: results
+        body: results,
+        status: currentStatus
     };
 }
