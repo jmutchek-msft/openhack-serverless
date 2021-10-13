e518e1c0a233fbff8c652af09ab79ba484c9aa6c
Steven St Jean <steve@sstjean.com>
2021-10-13 11:57:40 -0400

    testing pipeline with a change

--- challenge-3/functionapp/CreateRating/index.js
+++ challenge-3/functionapp/CreateRating/index.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+//test of deployment
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processing a request.');
     
