ba1903c6d3fc642564ce336fc56612411bd4851e
jmutchek <jmutchek@microsoft.com>
2021-10-13 11:21:48 -0400

    validate and store

--- challenge-3/functionapp/CreateRating/index.js
+++ challenge-3/functionapp/CreateRating/index.js
@@ -4,9 +4,8 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processing a request.');
     
     // set up locals
-    context.bindings.ratingsDocument = JSON.stringify({})
     var currentStatus = 0
-    var responseMessage = ""
+    var responseBody = ""
     
     // retrieve input payload elements
     const userId = req.body.userId;
@@ -34,34 +33,46 @@ module.exports = async function (context, req) {
     // e.g. https://serverlessohapi.azurewebsites.net/api/GetProduct?productId=75542e38-563f-436f-adeb-f426f1dabb5c
 
     url = `https://serverlessohapi.azurewebsites.net/api/GetProduct?productId=${productId}`
-    
-    await axios.get(url)
-        .then(function (response) {
-            context.log(response.data)
-            currentStatus = response.status
-        })
-        .catch(function (error) {
-            context.log(error.response.data);
-            currentStatus = 404
-        });    
+
+    if (currentStatus == 200) {
+        await axios.get(url)
+            .then(function (response) {
+                context.log(response.data)
+                currentStatus = response.status
+            })
+            .catch(function (error) {
+                context.log(error.response.data);
+                currentStatus = 404
+            });    
+        }
 
     // validate ratings is an integer between 0 and 5
     
-    if (rating < 0 || rating > 5) {
+    if (currentStatus == 200 && (rating < 0 || rating > 5)) {
         currentStatus = 400
-        responseMessage = "Only ratings from 0 to 5 are allowed."
+        responseBody = "Only ratings from 0 to 5 are allowed."
     }
 
     // build ratings document for cosmosDB
 
-    // return the entire review JSON payload with the newly created id and timestamp
+    if (currentStatus == 200) {
+        responseBody = JSON.stringify({
+            productId: productId,
+            userId: userId,
+            timestamp: Date.now(),
+            locationName: locationName,
+            rating: rating,
+            userNotes: userNotes
+        });
 
+        // return the entire review JSON payload with the newly created id and timestamp
+        context.bindings.ratingsDocument = responseBody
+    }
 
     // return the proper status code
-    // TODO
 
     context.res = {
         status: currentStatus,
-        body: responseMessage
+        body: responseBody
     };
 }
