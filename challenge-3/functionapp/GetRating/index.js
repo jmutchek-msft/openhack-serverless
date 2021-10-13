d169d3a2c84515b72bbfcdfad75b6a0faace8a82
ryglesiasms <53277959+ryglesiasms@users.noreply.github.com>
2021-10-13 10:49:15 -0600

    Update index.js

--- challenge-3/functionapp/GetRating/index.js
+++ challenge-3/functionapp/GetRating/index.js
@@ -1,9 +1,9 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const ratingid = (req.query.id || (req.body && req.body.id));
+    const ratingid = (req.query.ratingid || (req.body && req.body.ratingid));
 
-   var documents = context.bindings.retingid;
+   var documents = context.bindings.ratingid;
    var results = JSON.stringify(documents); 
 
     context.res = {       
