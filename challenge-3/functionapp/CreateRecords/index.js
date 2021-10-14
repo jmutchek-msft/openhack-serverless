f1ced0c4b7c2bb97fc2d9890b7900d3c76de7107
Ana Del Campo Mendizabal <andelcam@microsoft.com>
2021-10-14 15:46:44 -0400

    commit to 'main'

--- challenge-3/functionapp/CreateRecords/index.js
+++ challenge-3/functionapp/CreateRecords/index.js
@@ -2,19 +2,19 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processing a request.');
     
     // set up locals
-    var currentStatus = 200;
+   
+   // var currentStatus = 200;
     var responseBody = "Thank you! process completed... hopefully"   
    
     var saveThis = JSON.stringify(req.body.body);
-     
+    context.log (req)   ;
     context.bindings.mergedRecords = req.body.body; 
     context.log (saveThis)   ;
     responseBody = saveThis;
 
     // return the proper status code
 
-    context.res = {
-        status: currentStatus,
+    context.res = {       
         body: responseBody
     };
 }
